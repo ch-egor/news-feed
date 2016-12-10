0e168a786e34726e171e00e65c63bee994e988f3
ch-egor <ch_egor.92@mail.ru>
2016-12-10 19:12:18 +0700

    add pagination

--- src/AppBundle/Controller/NewsController.php
+++ src/AppBundle/Controller/NewsController.php
@@ -6,24 +6,38 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+
 class NewsController extends Controller
 {
     /**
-     * @Route("/news/", name="newsList")
+     * @Route("/feed/{page}", name="newsList")
      */
-    public function listAction(Request $request)
+    public function listAction($page = 1)
     {
+        if ($page < 1) {
+            throw new NotFoundHttpException();
+        }
         $repository = $this->getDoctrine()->getRepository('AppBundle:NewsItem');
-        $newsItems = $repository->findAll();
+        $limit = 5;
+        $query = $repository->createQueryBuilder('p')
+                ->getQuery()
+                ->setFirstResult($limit * ($page - 1))
+                ->setMaxResults($limit);
+        
+        $newsItems = new Paginator($query, $fetchJoinCollection = false);
+        $pageCount = ceil(count($newsItems) / $limit);
         return $this->render('news/list.html.twig', [
             'newsItems' => $newsItems,
+            'pageCount' => $pageCount,
         ]);
     }
     
     /**
      * @Route("/news/{urlName}", name="viewNewsItem")
      */
-    public function viewItemAction(Request $request, $urlName)
+    public function viewItemAction($urlName)
     {
         $repository = $this->getDoctrine()->getRepository('AppBundle:NewsItem');
         $newsItem = $repository->findOneByUrlName($urlName);
